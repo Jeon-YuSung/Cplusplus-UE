c4beefead72eef8041a6947f38a2d381d883f48a
Yu-Sung <120106925+ysungJ@users.noreply.github.com>
2025-01-29 23:08:11 +0900

    Create 2-11_Program.cpp

---
+++ CPP/Forouzan Cpp Bible/F_Chapter2/2-11_Program.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main (void){
+
+  bool x = 123; 
+  bool y = -8;
+  bool z = 0; 
+  bool t = -0;
+  bool u = true; 
+  bool v = false;
+
+  cout << "x 출력" << x << endl; 
+   cout << "y 출력" << y << endl; 
+   cout << "z 출력" << z << endl; 
+   cout << "t 출력" << t << endl; 
+   cout << "u 출력" << u << endl; 
+   cout << "v 출력" << v << endl; 
+  
+  return 0;
+}
