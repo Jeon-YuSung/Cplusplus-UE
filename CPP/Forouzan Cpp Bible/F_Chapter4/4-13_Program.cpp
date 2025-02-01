288b453964dc68e5785a59e15bc683f8a6bf017b
Vidan <120106925+ysungJ@users.noreply.github.com>
2025-02-01 22:40:04 +0900

    학점 기반으로 Pass/Non-pass출력하기

---
+++ CPP/Forouzan Cpp Bible/F_Chapter4/4-13_Program.cpp
@@ -0,0 +1,27 @@
+// 학점을 기반으로 PASS/ Non-pass 출력하기 
+
+#include <iostream>
+using namespace std;
+
+int main (void){
+
+  char grade;
+
+  cout << "학점을 입력하세요 : ";
+  cin >> grade;
+
+  switch(grade){
+    
+    case "A" : 
+  case "B" : 
+  case "C" : 
+      cout << " pass 입니다 " << endl;
+  case "D" : 
+  case "F" : 
+      cout << "non-pass (fail) 입니다 " << endl;
+  default :
+    cout << "입력 오류, 시도 하세요 " << endl;
+  }
+
+  return 0;
+}
