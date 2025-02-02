79ade6276ddd04be58e20c385f8aa61790fe8c36
Vidan <120106925+ysungJ@users.noreply.github.com>
2025-02-02 19:34:31 +0900

    EOF 제어 반복문으로 숫자의 합 구하기

---
+++ CPP/Forouzan Cpp Bible/F_Chapter5/5-6_Program.cpp
@@ -0,0 +1,17 @@
+// EOF 제어 While 반복문을 사용해 키보드로 입력한 숫자들의 합을 구하는 프로그램 
+#include <iostream>
+using namespace std;
+
+int main (void){
+
+  int sum = 0;
+  int n;
+  cout << "첫 번째 숫자를 입력하세요 (종료하려면 eof 입력) : ";
+while(cin >> num){
+
+  sum = sum + n;
+   cout << "다음 숫자를 입력 : " << endl; 
+}
+  cout << " 합 = " << sum << endl; 
+  return 0;
+}
