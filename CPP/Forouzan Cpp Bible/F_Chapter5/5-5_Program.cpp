734fbaa2ab34ce775107c171a67e2630c4dfcd91
Vidan <120106925+ysungJ@users.noreply.github.com>
2025-02-02 19:31:42 +0900

    센티넬 제어 반복문 예시

---
+++ CPP/Forouzan Cpp Bible/F_Chapter5/5-5_Program.cpp
@@ -0,0 +1,24 @@
+// 센티넬 제어 반복문을 사용해서 숫자들의 합 찾기
+
+#include <iostream>
+using namespace std;
+
+int main (void){
+
+  int sum = 0;
+  int n;
+
+  cout << "정수 입력 (종료하려면 -1 입력) : ";
+  cin >> n;
+
+  while(num != -1){
+sum = sum + num;
+  cout << " 정수 입력 하세요 (종료 숫자는 -1) : ";
+    cin >> num;
+  }
+
+  cout << " sum 의 합은 = " << sum <<endl; 
+  return 0;
+}
+
+ 
