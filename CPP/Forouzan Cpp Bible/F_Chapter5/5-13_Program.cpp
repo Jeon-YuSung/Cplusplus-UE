6433040e905551a77a678d6a278bb092e0d13462
Vidan <120106925+ysungJ@users.noreply.github.com>
2025-02-04 22:59:26 +0900

    데이터 유효성을 검사하는 프로그램

--- CPP/Forouzan Cpp Bible/F_Chapter5/5-13_Program.cpp
+++ CPP/Forouzan Cpp Bible/F_Chapter5/5-13_Program.cpp
@@ -1,4 +1,4 @@
-// do-while문을 ㅅ사용하여 데이터 유효성 검사에 사용하는 프로그램. 
+// do-while문을 사용하여 데이터 유효성 검사에 사용하는 프로그램. 
 #include <iostream>
 using namespace std;
 
@@ -17,22 +17,20 @@ int main (void){
   //조건 분기
   switch(score / 10){
     case 10 :
-    grade = "A";
-     break;
     case 9 :
-     grade = "A";
+     grade = 'A';
      break;
     case 8 :
-    grade = "B";
+    grade = 'B';
      break;
         case 7 :
-    grade = "C";
+    grade = 'C';
      break;
         case 6 :
-    grade = "D";
+    grade = 'D';
      break;
     default :
-     grade = "F";
+     grade = 'F';
     }
 
   cout << " 학점 = " << grade << endl; 
