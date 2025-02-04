2c46c9b03e8e9d1e06b15bfc8b3ee4003a178655
Vidan <120106925+ysungJ@users.noreply.github.com>
2025-02-04 22:27:00 +0900

    CallByReference

---
+++ CPP/Forouzan Cpp Bible/F_Chapter6/6-14.cpp
@@ -0,0 +1,27 @@
+/*
+callByReference확인 
+매개변수의 변경이 arugment에 영향을 준다는 것을 확인하는 프로그램 
+*/
+
+#include <iostream>
+using namespace std;
+
+/*
+fun 함수는 y로 참조를 전달 받음
+따라서  y는 함수 호출 때 argument의 별칭이 됨 
+매개변수를 변경하면 argumnet가 함께 변함
+*/
+int fun(int &y){
+
+  y++;
+  cout << "fun 함수 내부의 y = " << y << endl; 
+  return;
+}
+
+//메인 함수 
+int main (void){
+  int x = 10;
+  fun(x);
+cout << " 메인 함수 내부의 x = " << x << endl;
+  return 0;
+}
