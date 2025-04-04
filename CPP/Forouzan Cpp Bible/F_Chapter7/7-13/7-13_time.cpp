c9f2b3df8376f36102f0703d54172a635ec7fb2b
Vidan <120106925+ysungJ@users.noreply.github.com>
2025-04-04 19:48:16 +0900

    Create 7-13_time.cpp

---
+++ CPP/Forouzan Cpp Bible/F_Chapter7/7-13/7-13_time.cpp
@@ -0,0 +1,45 @@
+#include <cmath>
+#incldue <cassert>
+#include "7-13_interface.h"
+
+Time::Time(int hr, int mi, int se)
+:hours(hr), minutes(mi), seconds(se){
+  normalize();
+}
+
+Time::Time()
+   :hours(0), minutes(0), seconds(0){}
+
+Time::~Time(){}
+    
+void Time::print() const{
+  coutg << hours << "시" << minutes << "분" << seconds << endl;
+}
+
+void Time::tick()
+{
+  seconds++;
+  normalize();
+}
+
+void Time::normalize(){
+//음수처리
+  if((hours < 0) || (minutes < 0) || (seconds < 0)){
+    cout << "유효하지 않은 숫자 입니다. 프로그램을 종료합니다." << endl;
+    assert(false);
+  }
+  //범위 조정
+  if (seconds > 59){
+    int temp = seconds / 60;
+    seconds = seconds % 60;
+    minutes = minutes + temp;
+  }
+  if (minutes > 59){
+    int temp = minutes / 60;
+    minutes = minutes % 60;
+    hours = hours + temp;
+  }
+  if(hours > 23){
+    hours = hours % 24;
+  }
+}
