b409f10794f5d1d80f1915b8aed1b043ab856890
Vidan <120106925+ysungJ@users.noreply.github.com>
2025-04-04 19:17:12 +0900

    Update 7-10_interface.h

--- CPP/Forouzan Cpp Bible/F_Chapter7/7-10/7-10_interface.h
+++ CPP/Forouzan Cpp Bible/F_Chapter7/7-10/7-10_interface.h
@@ -1,6 +1,31 @@
 #include <iostream>
 using namespace std;
 
-#ifndef
-#define
+#ifndef 7-10_INTERFACE.H
+#define 7-10_INTERFACE.H
+  
+class Fraction{
+  private:
+int numer;
+int denom;
+
+//private 보조 멤버 함수
+void normalize();
+int gcd(int n, intm;
+public:
+  //생성자
+Fraction(int num, int den);
+Fraction();
+Fraction(const Fraction &fract);
+~Fraction;
+//접근자
+int getNumer() const;
+int getDenom() const;
+void print() const;
+//설정자
+void setNum(int num);
+void setDenom(int den);
+
+
+  };
 #endif
