70f62fc1374a86f804b49b5aa443b8a278def5ef
Vidan <120106925+ysungJ@users.noreply.github.com>
2025-04-04 19:40:49 +0900

    Update 7-13_interface.h

--- CPP/Forouzan Cpp Bible/F_Chapter7/7-13/7-13_interface.h
+++ CPP/Forouzan Cpp Bible/F_Chapter7/7-13/7-13_interface.h
@@ -1,1 +1,24 @@
 #include<iostream>
+using namespace std;
+
+#ifndef 7-13_INTERFACE.H
+#define 7-13_INTERFACE.H  
+
+class Time{
+  private:
+    int hours;
+    int minutes;
+    int seconds;
+
+    void normalize();
+
+  public:
+    Time(int hours, int minutes, int seconds);
+    Time();
+    ~Time();
+    
+    void print() const;
+    void tick();
+
+  };
+  #endif
