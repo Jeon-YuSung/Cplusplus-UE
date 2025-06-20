80a2a72fd41cc7e9edd03f02baf29b0f4a388511
Vidan <120106925+ysungJ@users.noreply.github.com>
2025-06-20 21:50:38 +0900

    Create FRACTION.h

---
+++ CPP/Forouzan Cpp Bible/F_Chapter9/9-3/FRACTION.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+#ifndef FRACTION_H
+#define FRACTION_H
+
+class Fraction {
+private:
+	int numer;
+	int denom;
+
+	//private 보조 멤버 함수
+	void normalize();
+	int gcd(int n, int m);
+
+public:
+	//생성자
+	Fraction(int num, int den);
+	Fraction();
+	Fraction(const Fraction& fract);
+	~Fraction();
+	//접근자
+	int getNumer() const;
+	int getDenom() const;
+	void print() const;
+	//설정자
+	void setNum(int num);
+	void setDenom(int den);
+
+
+
+};
+#endif
