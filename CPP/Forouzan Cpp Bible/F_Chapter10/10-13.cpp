a10845ea2b275af48081490608f351d2bb57de78
Vidan <120106925+ysungJ@users.noreply.github.com>
2025-06-23 18:50:23 +0900

    Create 10-13.cpp

---
+++ CPP/Forouzan Cpp Bible/F_Chapter10/10-13.cpp
@@ -0,0 +1,20 @@
+/*
+10-13. 입출력 연산자 사용하기 
+문자열 입출력 연산자를 사용하는 프로그램 
+*/
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+    
+    string strg;
+    cout << "문자열 입력 : ";
+    cin >> strg;
+    cout << strg << "\n";
+  
+
+    return 0;
+}
