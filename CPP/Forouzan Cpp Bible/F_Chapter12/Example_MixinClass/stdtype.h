122c0af0d867d2f43de854f3c2e863f86d719600
Vidan <120106925+ysungJ@users.noreply.github.com>
2025-06-28 13:01:56 +0900

    Create stdtype.h

---
+++ CPP/Forouzan Cpp Bible/F_Chapter12/Example_MixinClass/stdtype.h
@@ -0,0 +1,14 @@
+//12-8 stdtype 헤더파일
+#ifndef STDTYPE_H
+#define STDTYPE_H
+#include<iostream>
+using namespace std;
+
+class Stdtype {
+protected:
+	double gpa;
+public:
+	virtual void printGpa() = 0;
+};
+
+#endif // !1
