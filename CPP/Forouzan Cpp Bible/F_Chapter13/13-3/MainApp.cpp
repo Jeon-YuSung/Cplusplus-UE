a72180501ce5c355c9ae591a14461cb045b83ae0
Vidan <120106925+Jeon-YuSung@users.noreply.github.com>
2025-07-01 22:36:33 +0900

    Create MainApp.cpp

---
+++ CPP/Forouzan Cpp Bible/F_Chapter13/13-3/MainApp.cpp
@@ -0,0 +1,16 @@
+#include "smallest.h"
+#include <iostream>
+
+int main(void) {
+
+	Smallest small;
+	cout << " 지금까지 값중에 가장 작은 값 : " << small(4) << endl; 
+	cout << " 지금까지 값중에 가장 작은 값 : " << small(6) << endl;
+	cout << " 지금까지 값중에 가장 작은 값 : " << small(3) << endl;
+	cout << " 지금까지 값중에 가장 작은 값 : " << small(8) << endl;
+	cout << " 지금까지 값중에 가장 작은 값 : " << small(10) << endl;
+	cout << " 지금까지 값중에 가장 작은 값 : " << small(2) << endl;
+
+
+	return 0;
+} 
