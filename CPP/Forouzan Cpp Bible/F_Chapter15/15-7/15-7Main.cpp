09ba5851522f921b3ac4d8c04378deaf879f6226
Vidan <120106925+Jeon-YuSung@users.noreply.github.com>
2025-07-04 21:34:36 +0900

    Create 15-7Main.cpp

---
+++ CPP/Forouzan Cpp Bible/F_Chapter15/15-7/15-7Main.cpp
@@ -0,0 +1,9 @@
+#include "Smaller.h"
+
+int main(void)
+{
+	cout << "a와 B중 작은것 : " << smaller('a', 'B') << endl;
+	cout << "34와 256중 작은것 : " << smaller(34, 256) << endl;
+	cout << "23.45와 21.41중 작은것 : " << smaller(23.45, 21.41) << endl;
+
+}
