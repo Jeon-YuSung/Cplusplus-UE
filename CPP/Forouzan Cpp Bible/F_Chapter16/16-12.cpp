cc6149cb1c709753a6df271f40670458426ff809
Vidan <120106925+Jeon-YuSung@users.noreply.github.com>
2025-07-11 19:20:52 +0900

    Create 16-12.cpp

---
+++ CPP/Forouzan Cpp Bible/F_Chapter16/16-12.cpp
@@ -0,0 +1,22 @@
+//16-12. 파일에 있는 문자와 문자의 위치를 출력하는 프로그램 
+
+#include <iostream>
+#include<fstream>
+#include<cassert>
+using namespace std;
+
+int main(void) {
+
+	char ch;
+	int n;
+
+	ifstream istr;
+	istr.open("sample.txt", ios::in);
+	n = istr.tellg();
+	while(istr.get(ch)){
+		cout << n << " " << ch << endl;
+		n = istr.tellg();
+	}
+	istr.close();
+	
+	r
