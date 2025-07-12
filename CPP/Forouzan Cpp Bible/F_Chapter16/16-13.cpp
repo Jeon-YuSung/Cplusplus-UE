df46509af474b98ba8a59b4891e1f8c920c712e1
Vidan <120106925+Jeon-YuSung@users.noreply.github.com>
2025-07-12 23:57:40 +0900

    Create 16-13.cpp

---
+++ CPP/Forouzan Cpp Bible/F_Chapter16/16-13.cpp
@@ -0,0 +1,24 @@
+//16-13. 단어 뒤에 줄 바꿈 문자를 넣는 프로그램 
+
+#include <iostream>
+#include<fstream>
+#include<cassert>
+using namespace std;
+
+int main(void) {
+  fstream fstr;
+  fstr.open("16-13_file.txt", ios::in | ios::out):
+  char ch;
+
+  while(fstr.get(ch)){
+        
+    if(isspace(ch)){
+
+        fstr.seekp(-1, ios::cur);
+        fstr.put('\r');
+      }
+  }
+
+  fstr.close();
+	return 0;
+}
