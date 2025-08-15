885792d8952b37acebe5299ae935afd44cd5afa7
Vidan <120106925+Jeon-YuSung@users.noreply.github.com>
2025-08-15 22:45:12 +0900

    Create MediatorMainApp.cpp

---
+++ CPP/Forouzan Cpp Bible/F_Chapter20/Mediator Pattern/MediatorMainApp.cpp
@@ -0,0 +1,12 @@
+//20-16. 중재자 패턴 예제 애플리케이션 파일
+#include "mediator.h"
+using namespace std;
+
+int main(void) {
+
+    Mediator mediator;
+    mediator.sendMesage("안녕하세요! 여러분!");
+    return 0; 
+
+}
+
