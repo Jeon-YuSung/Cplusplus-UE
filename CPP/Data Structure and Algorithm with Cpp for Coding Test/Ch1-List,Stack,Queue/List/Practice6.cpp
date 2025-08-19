0803c933a87111e45982b28863285aff311dff32
Vidan <120106925+Jeon-YuSung@users.noreply.github.com>
2025-08-19 19:33:50 +0900

    Create Practice6.cpp

---
+++ CPP/Data Structure and Algorithm with Cpp for Coding Test/Ch1-List,Stack,Queue/List/Practice6.cpp
@@ -0,0 +1,20 @@
+//연습문제 6. list의 삽입 또는 삭제 함수 사용하기 
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <list>
+using namespace std;
+
+
+
+int main(void){
+	std::list<int> list1 = { 1, 2, 3, 4, 5 };
+	list1.push_back(6);                 // 1 2 3 4 5 6
+	list1.insert(next(list1.begin()), 0);  // 1 0 2 3 4 5 6
+	list1.insert(list1.end(), 7);        // 1 0 2 3 4 5 6 7
+	list1.pop_back();                     // 1 0 2 3 4 5 6
+
+	std::cout << "삽입 & 삭제 후 리스트: ";
+	for (auto i : list1)
+		cout << i << " ";
+}
